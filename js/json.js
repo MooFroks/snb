5fedcd0f5ec37a6be2442295998718bb2d81d670
superwoods <st_sister@icloud.com>
2015-08-19 15:06:21 +0800

    读取json功能

---
+++ js/json.js
@@ -0,0 +1,55 @@
+﻿/*
+ * json.js
+ * @BigKing 2015-08-10-15.00
+ * @St. 2015-07-26-06.53 begin
+ * @St. 2015-08-19-14.46
+**/
+var json = [
+    {
+        "name": "框架类",
+        "list": [
+            {
+                "name": "新华网技术部美编组<br>前端开发通用规范",
+                "txt": "这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字这是文字"
+            },
+			{
+                "name": "这是一个标题",
+                "txt": "demo/layout/index.html"
+            },
+			{
+                "name": "通用框架",
+                "txt": "demo/layout/index.html"
+            },
+			{
+                "name": "通用框架",
+                "txt": "demo/layout/index.html"
+            }
+        ]
+    },
+    {
+        "name": "文字类",
+        "list": [
+            {
+                "name": "文字类页面组件总览(@刘慧)",
+                "txt": "demo/module/accordion/shoufengqin.htm"
+            },
+            {
+                "name": "二级菜单2-2",
+                "txt": "http://www.163.com"
+            }
+        ]
+    },
+    {
+        "name": "图片类",
+        "list": [
+            {
+                "name": "二级菜单3-1",
+                "txt": "http://www.qq.com"
+            },
+            {
+                "name": "二级菜单3-2",
+                "txt": "http://www.baidu.com"
+            }
+        ]
+    }
+];
--- js/superNotebook.js
+++ js/superNotebook.js
@@ -1,12 +1,129 @@
-﻿//superNoteBook @St. 2015-07-26-06.53
+﻿/*
+ * superNoteBook.js
+ * @St. 2015-07-26-06.53 begin
+ * @St. 2015-08-19-14.46
+**/
+var $subNavName = $('#subNavName'),       //子目录标题栏名字
+	$subNavNameNUm = $('#subNavNameNUm'), //子目录标题栏数量
+	$markdownCtl = $('#markdownCtl'),     //markdown盒子
+	$markdownName = $('#markdownName'),   //markdown标题
+	$markdownTxt = $('#markdownTxt'),     //markdown内容
+	treeNav = {
+		init:function(para){
+			var def = {
+				o: json,      //obj json
+				m: "category",//mainNavId 分类目录id
+				s: "list"     //subNavId  子目录id
+			},
+			para = $.extend(def, para);
+			//
+			treeNav.onload(para);
+			treeNav.mainClick(para);
+			treeNav.subClick(para);
+			treeNav.resetFn(para);
+		},
+		onload:function(para){
+			var str = [[],[]];
+			for(var i=0; i < para.o.length; i++){
+				try{
+					// 一级
+					//if(typeof para.o[i]["url"] == "undefined"){
+						// 自身无链接
+						//str[0].push("<li class='li' id='nav"+i+"'><span class='t'>"+ para.o[i]["name"] +"</span></li>");
+						var li  = '<li class="li" id="nav'+i+'">'
+							+ '<span class="t">' + para.o[i]["name"] + '</span>'
+							+ ' <span class="n">(<span tag="num">' + para.o[i]["list"].length + '</span>)</span>'
+							+ '<button class="btn iconDel" id="del'+i+'">删除</button>'
+							+ '<button class="btn iconEdit" id="edit'+i+'">修改</button>'
+							+ '</li>';
+							
+						str[0].push(li);
+						//console.log(str[0]);
+						
+					//}//else{
+//						// 自身有链接
+//						str[0].push("<li class='li' id='nav"+i+"'><a target='contentIframe' class='t' href='"+para.o[i]["url"]+"'>"+ para.o[i]["name"] +"</a></li>");
+//					};
 
-/*
-这部分没有整理，现在是非常混乱的，
-昨天弄的太晚了，实在写不动了  =3=||
-等下午我睡醒了再搞一下吧。
-交互也还有一些问题。
-我睡会儿去。。。。
-*/
+
+					// 二级
+					if(para.o[i]["list"] != null){
+						//var subStr = "<ul id='nav"+i+'-2'+"'>";
+						var subStr = "<ul id='nav"+i+'-2'+"' class='hide'>";
+						for(var j=0; j<para.o[i]["list"].length; j++){
+							//subStr += "<li class='li'><div class='t'><a target='contentIframe' href="+ para.o[i]["list"][j]["url"] +">"+ para.o[i]["list"][j]["name"] +"</a></div></li>";
+							subStr += '<li class="li" id="item'+ j +'">',
+							subStr += '		<div class="t">' + para.o[i]["list"][j]["name"] + '</div>',
+							subStr += '		<div class="d">' + para.o[i]["list"][j]["txt"] + '</div>',
+							subStr += '		<button class="btn iconDel" id="listDel' + j + '">删除</button>',
+							subStr += '</li>'
+						};
+						subStr += "</ul>";
+						
+						str[1].push(subStr);
+					};
+				}catch(e){};
+			};
+			str[0] = str[0].join("");
+			str[1] = str[1].join("");
+			$("#" + para.m).html(str[0]);
+			$("#" + para.s).html(str[1]);
+		},
+		resetFn:function(para){
+			var n = 0,
+				$firstLi=$("#"+para.m).children().eq(n),
+				id = $firstLi.attr("id")+"-2",
+				$firstA=$("#"+id).show().find("li").eq(n);
+
+			$firstLi.addClass("on");
+			$firstA.addClass("on");
+			
+			$subNavName.html(para.o[n]["name"]);
+			$subNavNameNUm.html(para.o[n]["list"].length);
+			
+			$markdownName.html(para.o[n]["list"][n]["name"]);
+			$markdownTxt.html(para.o[n]["list"][n]["txt"]);
+		},
+		mainClick:function(para){
+			$("#" + para.m + " li").on("click",function(){
+				var $this = $(this),
+					id = $this.attr("id")+"-2",
+					$listUl = $("#"+id),
+					i = $this.index();
+
+				$this.addClass("on").siblings().removeClass("on");
+				$listUl.show().siblings().hide();
+				
+				//右侧导航栏标题ctl
+				$subNavName.html(para.o[i]["name"]);
+				$subNavNameNUm.html(para.o[i]["list"].length);
+
+			});
+		},
+		subClick:function(para){
+			$("#"+para.s+" li").on("click",function(){
+				var $this = $(this),
+					i = $this.parent().index();
+					j = $this.index();
+
+				$this.addClass("on").siblings().removeClass("on");
+				
+				//markdown
+				$markdownName.html(para.o[i]["list"][j]["name"]);
+				$markdownTxt.html(para.o[i]["list"][j]["txt"]);
+				
+			});
+		}
+	};
+
+treeNav.init({o: json, m: "category", s: "list"});//{obj: json, mainNavId: "category", subNavId: "list"}
+
+		
+
+
+
+
+
 
 
 var /*$showDelBtn = $('#iconDelCategory'),
